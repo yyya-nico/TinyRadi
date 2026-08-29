3c6b94d9966b292fad076378e46ddcd5189d2c66
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2026-08-30 01:22:35 +0900

    vite-plugin-singlefileを追加し1ファイル化、Viteの設定を更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
     build: {
         target: 'es2019',
         chunkSizeWarningLimit: 1000
-    }
+    },
+    plugins: [viteSingleFile()]
 });
